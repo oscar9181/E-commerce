d07832c92fa9775f3e8278c13470e6895b951466
oscar9181 <okolaoscar@gmail.com>
2025-01-12 17:17:09 +0300

    pillow installation

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def menupage(request):
+
+
+    return render(request ,'Ecommerce/menu.html',{})
